3449b996876e3ed5eb902ee0cccdc9d4c5660880
Manoel Valladao <valladao@gmail.com>
2021-06-16 07:16:08 -0300

    Add Express and Pug

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const base = require("airtable").base("appdfhl8Hj1EOYKhv")
+const express = require("express")
+const path = require("path")
 
-;(async () => {
+const app = express()
+app.set("view engine", "pug")
+app.set("views", path.join(__dirname, "."))
+
+app.get("/", async (req, res) => {
   const records = await base("Business Hours")
     .select({
       view: "Grid view",
     })
     .firstPage()
 
-  for (const record of records) {
-    console.log(record.get("Day"), record.get("Hours"))
-  }
-})()
+  res.render("page", {
+    records,
+  })
+})
+
+app.listen(3000, () => console.log("Server ready"))
