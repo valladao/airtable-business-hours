5881dd493b48c673ac282f5ca25cdf909032e1d0
Manoel Valladao <valladao@gmail.com>
2021-06-16 07:56:56 -0300

    Cache data

--- app.js
+++ app.js
@@ -6,16 +6,29 @@ const app = express()
 app.set("view engine", "pug")
 app.set("views", path.join(__dirname, "."))
 
-app.get("/", async (req, res) => {
-  const records = await base("Business Hours")
-    .select({
-      view: "Grid view",
-    })
-    .firstPage()
+let records
 
-  res.render("page", {
-    records,
-  })
+app.get("/", async (req, res) => {
+  if (records) {
+    console.log("used cache")
+    res.render("page", {
+      records,
+    })
+  } else {
+    records = await base("Business Hours")
+      .select({
+        view: "Grid view",
+      })
+      .firstPage()
+    console.log("cached")
+    res.render("page", {
+      records,
+    })
+    setTimeout(() => {
+      records = null
+      console.log("cache cleaned")
+    }, 10 * 1000)
+  }
 })
 
 app.listen(3000, () => console.log("Server ready"))
